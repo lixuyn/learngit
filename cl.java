863c927d2dcbda7e0619b919f22ee3cc92396d05
lixu <964203962@qq.com>
2016-04-09 12:43:13 +0800

    '提价了一个java文件'

---
+++ cl.java
@@ -0,0 +1,10 @@
+class Cl{
+	public static void main(String args[]){
+		double i =0.01;
+		for(i=0.01;i<=0.05;i+=0.01){
+			System.out.println(4/(i*i));
+			System.out.prinltn("git is a version controller");
+		}
+			
+	}	
+}
